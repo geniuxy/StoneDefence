e19a6a1907120fc003a70d56df3c760b867fc407
geniuxy <2238384910@qq.com>
2026-09-18 01:17:55 +0800

    初始构建角色动画框架

---
+++ Source/StoneDefence/Private/Anims/AnimInstances/SdAnimInstanceCharacterBase.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Anims/AnimInstances/SdAnimInstanceCharacterBase.h"
+
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void USdAnimInstanceCharacterBase::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	OwnerCharacter = Cast<ACharacter>(TryGetPawnOwner());
+	if (OwnerCharacter)
+	{
+		OwnerMovementComp = OwnerCharacter->GetCharacterMovement();
+	}
+}
+
+void USdAnimInstanceCharacterBase::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+
+	if (OwnerCharacter)
+	{
+		FVector Velocity = OwnerCharacter->GetVelocity();
+		Speed = Velocity.Length();
+	}
+
+	if (OwnerMovementComp)
+	{
+		bIsInAir = OwnerMovementComp->IsFalling();
+	}
+}
---
+++ Source/StoneDefence/Public/Anims/AnimInstances/SdAnimInstanceCharacterBase.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SdAnimInstanceBase.h"
+#include "SdAnimInstanceCharacterBase.generated.h"
+
+class UCharacterMovementComponent;
+/**
+ * 
+ */
+UCLASS()
+class STONEDEFENCE_API USdAnimInstanceCharacterBase : public USdAnimInstanceBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+	UFUNCTION(BlueprintPure, meta=(BlueprintThreadSafe))
+	FORCEINLINE bool GetIsInAir() const { return bIsInAir; }
+
+	UFUNCTION(BlueprintPure, meta=(BlueprintThreadSafe))
+	FORCEINLINE float GetSpeed() const { return Speed; }
+
+protected:
+	UPROPERTY()
+	ACharacter* OwnerCharacter;
+
+	UPROPERTY()
+	UCharacterMovementComponent* OwnerMovementComp;
+	
+	bool bIsInAir = false;
+	float Speed;
+};
